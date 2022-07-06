612c531f85472406c459f709f7981857a7820296
enhyq <54441339+enhyq@users.noreply.github.com>
2022-07-06 18:32:13 +0900

    Create Autocomplete.cpp

---
+++ Week2/TrieBasedAutocompletion/Autocomplete.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct node {
+    bool terminal;
+    int score;
+};
+
+class Trie {
+    private:
+
+    public:
+
+};
+
+
+int main(int argc, char const *argv[])
+{
+    
+    
+
+
+
+    return 0;
+}

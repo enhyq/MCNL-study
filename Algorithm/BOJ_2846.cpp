f1b301c6cddd02dca310475d4ea3807b652cb586
enhyq <54441339+enhyq@users.noreply.github.com>
2022-07-12 17:00:52 +0900

    AC

--- Algorithm/BOJ_2846.cpp
+++ Algorithm/BOJ_2846.cpp
@@ -1,6 +1,6 @@
 /**
  * @author Eunhyeok Kwon
- * @date 2022-07-11 ~
+ * @date 2022-07-11 ~ 2022-07-12
  * @link https://www.acmicpc.net/problem/2846
  */
 
@@ -12,25 +12,27 @@ int main(int argc, char const *argv[])
 {
     /* code */
     // cur is at max 1000 in this problem
-    int N, cur, prev=0, begin, max=0;
+    int N, cur, prev=0, begin=1000, max=0;
     cin >> N;
     for(int i=0; i<N; i++) {
         cin >> cur;
-        cout << "cur: " << cur << " ";
-        cout << "prev:" << prev << " ";
-        cout << "begin:" << begin << endl;
-        if(i==0) {
-            begin = cur;
+        // cout << "cur: " << cur << " ";
+        // cout << "prev:" << prev << " ";
+        // cout << "begin:" << begin << endl;
+        if(i == 0) {
+            begin = cur;                                // initial condition
         }
-        else if(cur <= prev){
-            begin = cur;               // reset prev
+        else {
+            if(cur <= prev){
+                begin = cur;                            // reset begin if cur <= prev
+            }
+            if(cur-begin > max) {                       // check for new max
+                max = cur-begin;
+                // cout << "new max: " << max << endl;
+            } 
         }
-        if(cur-begin > max) {
-            max = cur-begin;
-            cout << "new max: " << max << endl;
-        }
-        prev = cur;
+        prev = cur;                                     // set prev to cur
     }
-    cout << max;
+    cout << max << endl;
     return 0;
 }

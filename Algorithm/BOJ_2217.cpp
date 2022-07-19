698e667af4f81fbfb881d6ebfd05124a16576e3a
enhyq <54441339+enhyq@users.noreply.github.com>
2022-07-19 10:31:09 +0900

    AC BOJ_2217

--- Algorithm/BOJ_2217.cpp
+++ Algorithm/BOJ_2217.cpp
@@ -1,9 +1,11 @@
 /**
  * @author Eunhyeok Kwon
- * @date 2022-07-18 ~ !
+ * @date 2022-07-18 ~ 2022-07-19
  * @link https://www.acmicpc.net/problem/2217
  */
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,13 +15,28 @@ int main(int argc, char const *argv[])
     // 2. each rope has strength
     // 3. when using multiple ropes together, weight is evenly distributed among used ropes
     // 4. find max weight that can be lifted using given ropes (doesn't have to use all ropes)
-    
-    int N, i, max_w=0, sum=0, temp;
+
+    // N개의 로프 1 ≤ N ≤ 100,000
+    // 각 로프가 버틸 수 있는 최대중량 0 < K < 10,000
+    int N, i, max_w = 0, weight = 0, temp;
+    vector<int> ropes;
+
     cin >> N;
-    for(i=0; i<N; i++) {
+
+    for (i = 0; i < N; i++)
+    { // get input
         cin >> temp;
-        // if(temp > (sum+temp)
+        ropes.push_back(temp);
     }
+    sort(ropes.begin(), ropes.end()); // sort in descending order
+
+    for (int i = 0; i < N; i++)
+    {
+        weight = ropes[i] * (N - i);
+        if (weight > max_w)
+            max_w = weight;
+    }
+    cout << max_w << endl;
 
     return 0;
 }
